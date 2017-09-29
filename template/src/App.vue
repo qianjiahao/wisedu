ff1dc60a9f4f9868de8e5d1bfe77a81a49f09392
qianjiahao <1041897388@qq.com>
2017-09-29 15:03:07 +0800

    create template

---
+++ template/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+html, body, #app {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #f9f9f9;
+  font-family: ZhiFont, ".SFNSText-Regular", "Helvetica Neue", Roboto, "Segoe UI", sans-serif;
+}
+</style>
---
+++ template/src/config/api.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import host from './host'
+const api = {}
+
+const instance = axios.create({
+  baseURL: host.baseURL
+})
+
+instance.interceptors.request.use(function (config) {
+  return config
+})
+
+instance.interceptors.response.use(function (response) {
+  return response
+})
+
+api.get = function (url, params = {}, config = {}) {
+  return instance({
+    method: 'GET',
+    url,
+    params,
+    ...config
+  })
+}
+
+api.post = function (url, data = {}, config = {}) {
+  return instance({
+    method: 'POST',
+    url,
+    data,
+    ...config
+  })
+}
+
+export default api
+//
+// function serialize (data) {
+//   if (!data) return ''
+//   var pairs = [], value
+//   for (var name in data) {
+//     if (!data.hasOwnProperty(name)) continue
+//     if (typeof data[name] === 'function') continue
+//     value = data[name].toString()
+//     name = encodeURI(name)
+//     value = encodeURI(value)
+//     pairs.push(name + '=' + value)
+//   }
+//   if (pairs.length) {
+//     return '?' + pairs.join('&')
+//   } else {
+//     return ''
+//   }
+// }
